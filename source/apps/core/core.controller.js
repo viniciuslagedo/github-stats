adf3197a27c08f443e3bb0ea3a791e8b39b88d33
Vinícius Lagêdo <vinicius@hadrons.io>
2016-12-06 00:40:19 -0200

    Angular initial structure

---
+++ source/apps/core/core.controller.js
@@ -0,0 +1,12 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('CoreController', CoreController);
+
+    /* @ngInject */
+    function CoreController () {
+        var vm = this;
+    }
+}());
